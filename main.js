9e19171b0d359ac4bec6791a5f43028b70706f2b
Abdul/AQ <abdulkadir.bello@dynamowit.com>
2022-03-11 22:06:54 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const finishBtn = document.querySelector('#finish')
+const innerDivs = document.querySelectorAll('.progress')
+
+// eventlistener for button click
+finishBtn.addEventListener('click', () => {
+    let output = 50;
+    let interval = setInterval(run, 10);
+
+    function run() {
+        for (i = 0; i <= innerDivs.length; i++) {
+            innerDivs[i].textContent = `${output}%`;
+            innerDivs[i].style.width = `${output}%`;
+            if (output != 100) {
+                output++;
+            }else{
+                clearInterval(interval);
+            }
+        }
+    }
+});
